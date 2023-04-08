0c3dc05461bbea38d8bd0e5d89c85f1f24024190
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-04-08 17:59:51 +0200

    include serviceworker in dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import html from '@web/rollup-plugin-html';
 import { terser } from 'rollup-plugin-terser';
 import OMT from '@surma/rollup-plugin-off-main-thread';
+import copy from 'rollup-plugin-copy';
 
 export default {
     input: './public_html/index.html',
@@ -8,6 +9,11 @@ export default {
     plugins: [
         html({ include: '**/*.html', minify: false }),
         OMT(),
-        terser()
+        terser(),
+        copy({
+            targets: [
+                { src: 'public_html/serviceworker.js', dest: 'dist/' },
+            ]
+        })
     ]
 };
