4d0818e666ab3540a058925c074d3033e3e53c86
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-04-12 21:49:15 +0200

    separate card for fetching exchange rates

--- public_html/storage/storage-page.component.html.js
+++ public_html/storage/storage-page.component.html.js
@@ -15,17 +15,24 @@ export default /*html*/ `<div class="card">
         <button class="btn btn-primary" id="syncbutton">Synchronize</button>
         <button class="btn btn-primary" id="deletelocaldatabutton">Delete local data</button>
     </div>
+</div>
+<br />
+<div class="card">
     <div class="card-header">Fetch exchange rates</div>
     <div class="card-body">
         <p>Fetch exchange rates from various sources and store in your git repository</p>
-        <button class="btn btn-primary" id="fetchnearusdbutton">Fetch NEAR/USD rates from nearblocks.io</button>
         
+        <p>
+        <button class="btn btn-primary" id="fetchnearusdbutton">Fetch NEAR/USD rates from nearblocks.io</button>        
         <button class="btn btn-primary" id="fetchusdnokbutton">Fetch USD/NOK rates from Norges Bank</button>
+        </p>
 
+        <br />
         <h3>Yahoo finance</h3>
         <p>Download historical prices from <a href="https://finance.yahoo.com/quote/NEAR-USD/history">https://finance.yahoo.com/quote/NEAR-USD/history</a>, and upload the file here</p>
 
         <input type="file" id="yahoofinancecsvfileinput" class="form-control" placeholder="Upload Yahoo CSV">
         <button class="btn btn-primary" id="importnearusdyahoobutton">Import NEAR/USD rates from Yahoo finance</button>
     </div>
-</div>`;
+</div>
+<br />`;
