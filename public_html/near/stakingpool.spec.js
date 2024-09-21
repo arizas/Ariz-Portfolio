862ed6ef8ca0d75697c532515da3d9f2ea9d8a77
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-09-21 17:36:07 +0200

    adjusted test timeouts

--- public_html/near/stakingpool.spec.js
+++ public_html/near/stakingpool.spec.js
@@ -19,6 +19,7 @@ describe('stakingpool', () => {
         expect(blockdata.header).to.deep.equal(blockInfo.header);
     });
     it('should fetch staking balances', async function () {
+        this.timeout(5 * 60_000);
         const account_id = 'psalomo.near';
         const stakingpool_id = '01node.poolv1.near';
 
