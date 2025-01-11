d5072cd27e1b3214f51c516fdb0834df0a02c327
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-01-11 18:14:38 +0100

    set locale for test

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -40,7 +40,7 @@ export default {
     },
   })],
   testRunnerHtml: testRunnerImport =>
-    `<html lang="en-US">
+    `<html>
       <body>
         <script type="module">
             import { expect, assert} from 'https://cdn.jsdelivr.net/npm/chai@5.0.0/+esm';
