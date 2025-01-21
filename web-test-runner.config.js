fa9f3ef9b527bd928b4cf47bbd01a374f5aa2437
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-01-21 08:49:25 +0100

    use environment variable for storing in archive rpc cache

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -19,7 +19,7 @@ const importMapStart = indexHtml.indexOf('<script type="importmap">') + '<script
 const importMapEnd = indexHtml.indexOf('</script>', importMapStart);
 const importMap = JSON.parse(indexHtml.substring(importMapStart, importMapEnd));
 
-const storeInArchiveRpcCache = true;
+const storeInArchiveRpcCache = process.env.STORE_IN_ARCHIVE_RPC_CACHE;
 
 export default {
   files: [
